622a60c21c5bf58c98dd6bdb2a1993b994930d10
Johhny Hasse <jhasse20@hotmail.com>
2024-02-22 22:31:38 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { config } from "dotenv";
+
+config();
+
+console.log(".ENV API PORT: ", process.env.API_PORT);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
